ad7ee9b90616784183e6ec6a2641618aaa5b9871
Kofi Morrison <kaymaux@gmail.com>
2022-01-04 01:53:55 +0100

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,17 @@ function App() {
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <div>
-          <Routes>
-          <Route exact path='/' element={<Fib/>}/>
-          <Route path='/otherpage' element={<OtherPage/>}/>
+        <h1>Fib Calculator</h1>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
+      </header>
+      <div>
+        <Routes>
+        <Route exact path='/' element={<Fib/>}/>
+        <Route path='/otherpage' element={<OtherPage/>}/>
 
-          </Routes>
-        </div>
+        </Routes>
+      </div>
     </div>
     </Router>
     
